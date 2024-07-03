e510edc19f9c3c81c12395c14b8e1b96229e33ac
Saulo Chiosi <sauloc.1234@gmail.com>
2024-07-03 15:17:30 -0300

    Predicate

---
+++ src/estudo/predicate1/ProductPredicate.java
@@ -0,0 +1,13 @@
+package estudo.predicate1;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product>{
+
+	@Override
+	public boolean test(Product p) {
+		
+		return p.getPrice() >= 100;
+	}
+
+}
