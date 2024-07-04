d99a1681e1262ecebbd31d512d6bd97c63b9e091
Saulo Chiosi <sauloc.1234@gmail.com>
2024-07-03 23:22:12 -0300

    Function - Expressão lambda inline - REMOVENDO IMPORT

--- src/estudo/function/Program.java
+++ src/estudo/function/Program.java
@@ -3,7 +3,6 @@ package estudo.function;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
